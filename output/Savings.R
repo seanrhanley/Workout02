3d83f93ffc4c902034a57713f69b32ba531e9b04
Sean Hanley <sean.hanley@berkeley.edu>
2019-04-09 00:52:50 -0700

    Made changes to app code

--- output/Savings.R
+++ output/Savings.R
@@ -12,37 +12,66 @@ library(shiny)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  fluidRow(
+    # Application title
+    titlePanel("Three Ways to Save Money")
+  ),
+  fluidRow(
+    column(4, 
+           sliderInput("init", "Initial Amount",
+                       min = 0,
+                       max = 100000,
+                       step = 500,
+                       value = 1000),
+           sliderInput("contrib",
+                       "Annual Contribution",
+                       min = 0,
+                       max = 50000,
+                       step = 500,
+                       value = 2000)
+           ),
+    column(4,
+           sliderInput("return_rate",
+                       "Return Rate (in %)",
+                       min = 0,
+                       max = 20,
+                       step = 0.1,
+                       value = .05),
+           sliderInput("growth_rate",
+                       "Growth Rate (in %)",
+                       min = 0,
+                       max = 20,
+                       step = 0.1,
+                       value = 0.2)
+           ),
+    column(4,
+           sliderInput("years",
+                       "Years",
+                       min = 0,
+                       max = 50,
+                       step = 1,
+                       value = 20),
+           selectInput("facet_choice", 
+                       "Facet?",
+                       c('Yes', 'No'),
+                       selected = 'No',
+                       multiple = FALSE,
+                       selectize = TRUE)
+           )
+    ),
+  fluidRow(
+    tags$hr()
+    ##<hr></hr>
+  )
 )
 
+
+   
+  
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+   
 }
 
 # Run the application 
