1d610b413ae64ef12868e7c9e54e891f16405c7c
Sean Hanley <sean.hanley@berkeley.edu>
2019-04-15 23:09:49 -0700

    Added script with functions used to create data frame of investments

--- output/Savings.R
+++ output/Savings.R
@@ -1,13 +1,5 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+source("InvestmentFunctions.R")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -36,13 +28,13 @@ ui <- fluidPage(
                        min = 0,
                        max = 20,
                        step = 0.1,
-                       value = .05),
+                       value = 5),
            sliderInput("growth_rate",
                        "Growth Rate (in %)",
                        min = 0,
                        max = 20,
                        step = 0.1,
-                       value = 0.2)
+                       value = 2)
            ),
     column(4,
            sliderInput("years",
@@ -62,16 +54,71 @@ ui <- fluidPage(
   fluidRow(
     tags$hr()
     ##<hr></hr>
+  ),
+  fluidRow(
+      h4("Balances"),
+      tableOutput("modular")
   )
 )
 
 
    
   
-# Define server logic required to draw a histogram
+# Define server logic required to construct output graphs
 server <- function(input, output) {
    
+  mods <- reactive({
+    
+    #create initial vectors for years for each mode, including year 0
+    no_contrib <- rep(0, input$years + 1)
+    fixed_contrib <- rep(0, input$years + 1)
+    growing_contrib <- rep(0, input$years + 1)
+    
+    #initial variables
+    i_inv <- input$init
+    rate <- input$return_rate / 100
+    growth <- input$growth_rate / 100
+    contrib <- input$contrib
+    years <- input$years
+    
+    for (i in 1:years){
+      
+      if (i == 1){
+        
+        no_contrib[i] <- i_inv
+        fixed_contrib[i] <- i_inv
+        growing_contrib[i] <- i_inv
+        
+      }
+      
+      #mode1
+      no_contrib[i+1] <- signif(future_value(amount = i_inv, rate = rate, years = i), 7)
+      #mode2
+      fixed_contrib[i+1] <- signif(annuity(contrib = contrib, rate = rate, years = i) + no_contrib[i+1], 7)
+      #mode3
+      growing_contrib[i+1] <- signif(growing_annuity(contrib = contrib, rate = rate, growth = growth, years = i) + no_contrib[i+1], 7)
+      
+    }
+    
+    #create data frame of different modalities
+    mods <- data.frame(
+      year = seq(0, years, 1),
+      'No Contribution' = no_contrib,
+      'Fixed Contribution' = fixed_contrib,
+      'Growing Contribution' = growing_contrib
+    )
+    
+    return(mods)
+    
+  })
    
+    output$modular <- renderTable({
+      
+      data <- mods()
+      head(data, n = isolate(input$years + 1))
+      
+    })
+  
 }
 
 # Run the application 
